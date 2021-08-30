1db87b544596525e1f79af92c8477e4151a26784
Matt Liu <mattliuym@gmail.com>
2021-08-30 22:27:32 +1200

    made some updates

--- src/Components/Users/Register/index.js
+++ src/Components/Users/Register/index.js
@@ -1,42 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import 'antd/dist/antd.css';
 import './index.css';
 import { Form, Input, Select, Button} from 'antd';
 const { Option } = Select;
-// const residences = [
-//     {
-//         value: 'zhejiang',
-//         label: 'Zhejiang',
-//         children: [
-//             {
-//                 value: 'hangzhou',
-//                 label: 'Hangzhou',
-//                 children: [
-//                     {
-//                         value: 'xihu',
-//                         label: 'West Lake',
-//                     },
-//                 ],
-//             },
-//         ],
-//     },
-//     {
-//         value: 'jiangsu',
-//         label: 'Jiangsu',
-//         children: [
-//             {
-//                 value: 'nanjing',
-//                 label: 'Nanjing',
-//                 children: [
-//                     {
-//                         value: 'zhonghuamen',
-//                         label: 'Zhong Hua Men',
-//                     },
-//                 ],
-//             },
-//         ],
-//     },
-// ];
+
 const formItemLayout = {
     labelCol: {
         xs: {
